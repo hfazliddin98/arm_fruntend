65e5bb61c4ebdc4a52a3a0ba750e83038f85c4bd
AzimjonMeliboev <azimeliboev2000@gmail.com>
2023-02-19 22:46:42 +0500

    Update 01

--- js/app.js
+++ js/app.js
@@ -3,52 +3,59 @@ const inpId = document.getElementById('inp-id')
 const inpPassw = document.getElementById('inp-passw')
 const btn = document.getElementById('btn')
 const forms = document.querySelectorAll('#form')
-const noneBe1 = document.querySelector('.none_1')
-const noneBe2 = document.querySelector('.none_2')
+const noneBe1 = document.querySelector('#none_1')
+const noneBe2 = document.querySelector('#none_2')
+const eye = document.querySelector('#eye_pass')
 
 //regEX
-// const regEX_id = /^[0-9]{12}$/
-// const regEX_passw = /^[a-zA-Z0-9]{6,12}$/
+const regEX_id = /^[0-9]{12}$/;
+const regEX_passw = /^[a-zA-Z0-9]{6,12}$/;
+const regEX_boshH = /^[a-zA-Z]$/;
 
-
-function name() {
-
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        form.classList.add('was-validated')
+    //input Id
+inpId.addEventListener('keyup', e => {
+    if (regEX_id.test(inpId.value)){
+        inpId.classList.add('is-valid')
+        inpId.classList.remove('is-invalid')
         noneBe1.classList.add('d-none')
+    }else{
+        inpId.classList.add('is-invalid')
+        inpId.classList.remove('is-valid')
+        noneBe1.classList.add('d-none')
+    }
+})
+
+    //input Password
+inpPassw.addEventListener('keyup', e => {
+    if (regEX_boshH.test(inpPassw.value.charAt())) {
+        if (regEX_passw.test(inpPassw.value)){
+            inpPassw.classList.add('is-valid')
+            inpPassw.classList.remove('is-invalid')
+            noneBe2.classList.add('d-none')
+            eye.classList.remove('d-none')
+            eye.classList.add('d-inline-block')
+        }else{
+            inpPassw.classList.add('is-invalid')
+            inpPassw.classList.remove('is-valid')
+            noneBe2.classList.add('d-none')
+            eye.classList.remove('d-none')
+            eye.classList.add('d-inline-block')
+        }
+    }else{
+        inpPassw.classList.add('is-invalid')
         noneBe2.classList.add('d-none')
-
-      }, false)
-    })
-}
-name()
-
-
-
-// let yoqQil = (e) => {
-//   if (forms.classList.value == 'was-validated') {
-//     console.log('Hello');
-//   })
-// }
-  
-// noneBe1.classList.add('d-none')
-
-
-
-
-
-
-
-
-
-
-
-
+        eye.classList.remove('d-none')
+        eye.classList.add('d-inline-block')
+    }
+})
+    //icon eye
+eye.addEventListener('click', e => {
+    if(inpPassw.getAttribute('type') == "password"){
+        inpPassw.setAttribute('type', 'text')
+    }else if(inpPassw.getAttribute('type') == "text"){
+        inpPassw.setAttribute('type', 'password')
+    }
+})
 
 
 
